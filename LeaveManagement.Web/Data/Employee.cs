3a2f61466e7615e0fa22b51bfa17bac083649830
acer <acer@DESKTOP-5KHJIKA>
2023-08-14 19:57:56 +0530

    Add project files.

---
+++ LeaveManagement.Web/Data/Employee.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LeaveManagement.Web.Data
+{
+    public class Employee : IdentityUser
+    {
+        public String? Firstname { get; set; }
+        public String? Lastname { get; set; }
+        public String? TexId { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateJoined { get; set; }
+
+    }
+}
---
+++ LeaveManagement.Web/Data/LeaveType.cs
@@ -0,0 +1,9 @@
+﻿namespace LeaveManagement.Web.Data
+{
+    public class LeaveType : BaseEntity
+    {
+        public String Name { get; set; }
+        public int DefaultDays { get; set; }
+        
+    }
+}
